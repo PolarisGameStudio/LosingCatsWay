95c05f2ce2cb0422940bf47427cb5d645264a529
crkteam <39040121+crkteam@users.noreply.github.com>
2023-02-20 12:17:54 +0800

    123

--- Assets/Script/Login/Login.cs
+++ Assets/Script/Login/Login.cs
@@ -133,8 +133,8 @@ public class Login : MyApplication
     public void LoginByApple()
     {
         Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-        var rawNonce = GenerateRandomString(32);
-        var nonce = GenerateSHA256NonceFromRawNonce(rawNonce);
+        var rawNonce = MathfExtension.GenerateRandomString(32);
+        var nonce = MathfExtension.GenerateSHA256NonceFromRawNonce(rawNonce);
 
         var loginArgs = new AppleAuthLoginArgs(LoginOptions.IncludeEmail | LoginOptions.IncludeFullName, nonce);
 
@@ -255,61 +255,5 @@ public class Login : MyApplication
     {
         system.soundEffect.Play("ED00004");
         controller.settings.Open();
-    }
-
-    private string GenerateRandomString(int length)
-    {
-        if (length <= 0)
-        {
-            throw new Exception("Expected nonce to have positive length");
-        }
-
-        const string charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._";
-        var cryptographicallySecureRandomNumberGenerator = new RNGCryptoServiceProvider();
-        var result = string.Empty;
-        var remainingLength = length;
-
-        var randomNumberHolder = new byte[1];
-        while (remainingLength > 0)
-        {
-            var randomNumbers = new List<int>(16);
-            for (var randomNumberCount = 0; randomNumberCount < 16; randomNumberCount++)
-            {
-                cryptographicallySecureRandomNumberGenerator.GetBytes(randomNumberHolder);
-                randomNumbers.Add(randomNumberHolder[0]);
-            }
-
-            for (var randomNumberIndex = 0; randomNumberIndex < randomNumbers.Count; randomNumberIndex++)
-            {
-                if (remainingLength == 0)
-                {
-                    break;
-                }
-
-                var randomNumber = randomNumbers[randomNumberIndex];
-                if (randomNumber < charset.Length)
-                {
-                    result += charset[randomNumber];
-                    remainingLength--;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private string GenerateSHA256NonceFromRawNonce(string rawNonce)
-    {
-        var sha = new SHA256Managed();
-        var utf8RawNonce = Encoding.UTF8.GetBytes(rawNonce);
-        var hash = sha.ComputeHash(utf8RawNonce);
-
-        var result = string.Empty;
-        for (var i = 0; i < hash.Length; i++)
-        {
-            result += hash[i].ToString("x2");
-        }
-
-        return result;
     }
 }
--- Assets/Script/Tool/Extension/MathfExtension.cs
+++ Assets/Script/Tool/Extension/MathfExtension.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public static class MathfExtension
 {
@@ -131,5 +135,61 @@ public static class MathfExtension
     {
         start = Mathf.FloorToInt(value / 10f) * 10 + 1;
         end = start + 9;
+    }
+    
+    public static string GenerateRandomString(int length)
+    {
+        if (length <= 0)
+        {
+            throw new Exception("Expected nonce to have positive length");
+        }
+
+        const string charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._";
+        var cryptographicallySecureRandomNumberGenerator = new RNGCryptoServiceProvider();
+        var result = string.Empty;
+        var remainingLength = length;
+
+        var randomNumberHolder = new byte[1];
+        while (remainingLength > 0)
+        {
+            var randomNumbers = new List<int>(16);
+            for (var randomNumberCount = 0; randomNumberCount < 16; randomNumberCount++)
+            {
+                cryptographicallySecureRandomNumberGenerator.GetBytes(randomNumberHolder);
+                randomNumbers.Add(randomNumberHolder[0]);
+            }
+
+            for (var randomNumberIndex = 0; randomNumberIndex < randomNumbers.Count; randomNumberIndex++)
+            {
+                if (remainingLength == 0)
+                {
+                    break;
+                }
+
+                var randomNumber = randomNumbers[randomNumberIndex];
+                if (randomNumber < charset.Length)
+                {
+                    result += charset[randomNumber];
+                    remainingLength--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static string GenerateSHA256NonceFromRawNonce(string rawNonce)
+    {
+        var sha = new SHA256Managed();
+        var utf8RawNonce = Encoding.UTF8.GetBytes(rawNonce);
+        var hash = sha.ComputeHash(utf8RawNonce);
+
+        var result = string.Empty;
+        for (var i = 0; i < hash.Length; i++)
+        {
+            result += hash[i].ToString("x2");
+        }
+
+        return result;
     }
 }
