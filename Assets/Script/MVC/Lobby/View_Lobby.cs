5c20f9ffd4e820620aca3036d988c7501c36b539
crkteam <39040121+crkteam@users.noreply.github.com>
2023-02-26 10:58:45 +0800

    改版成功

--- Assets/Script/MVC/Lobby/View_Lobby.cs
+++ Assets/Script/MVC/Lobby/View_Lobby.cs
@@ -32,23 +32,19 @@ public class View_Lobby : ViewBehaviour
 
     [Title("Tween-Right")] public RectTransform tip;
 
-    [Title("TopRight")]
-    public TextMeshProUGUI nextLevelBestItemText;
+    [Title("TopRight")] public TextMeshProUGUI nextLevelBestItemText;
     public TextMeshProUGUI nextdLevelText;
     [SerializeField] private GameObject nextRewardChat;
     [SerializeField] private TextMeshProUGUI nextQuestText;
     [SerializeField] private GameObject nextQuestChat;
-    
-    [Title("CardCatNotifies")] 
-    public Transform cardCatNotifyContent;
 
-    [Title("Particle")]
-    public UIParticle expParticle;
+    [Title("CardCatNotifies")] public Transform cardCatNotifyContent;
+
+    [Title("Particle")] public UIParticle expParticle;
     public UIParticle moneyParticle;
     public UIParticle diamondParticle;
 
-    [Title("Red")]
-    public GameObject lobbyDailyQuestRed;
+    [Title("Red")] public GameObject lobbyDailyQuestRed;
     public Animator lobbyDailyQuestRedAnimator;
     public GameObject archiveRedPoint;
     public Animator archiveRedPointAnimator;
@@ -64,11 +60,13 @@ public class View_Lobby : ViewBehaviour
     public Animator friendRedPointAnimator;
     public GameObject lobbyLevelRewardRed;
     public Animator lobbyLevelRewardRedAnimator;
+    public GameObject eventRedPoint;
+    public Animator eventRedPointAnimator;
 
     public override void Open()
     {
         catCountText.text = App.system.cat.GetCats().Count.ToString("00");
-        
+
         UIView.InstantShow();
 
         // Top
@@ -79,17 +77,19 @@ public class View_Lobby : ViewBehaviour
 
         Vector2 buildButtonPos = buildButton.anchoredPosition + new Vector2(30, 30);
         for (int i = 0; i < downButtons.Length; i++)
-            downButtons[i].DOAnchorPos(downButtonsOrigins[i], 0.25f).From(buildButtonPos).SetEase(Ease.OutBack).SetDelay(0.25f + i * 0.0625f);
+            downButtons[i].DOAnchorPos(downButtonsOrigins[i], 0.25f).From(buildButtonPos).SetEase(Ease.OutBack)
+                .SetDelay(0.25f + i * 0.0625f);
 
         // Left
         for (int i = 0; i < leftButtons.Length; i++)
             leftButtons[i].DOLocalMoveX(45, 0.25f).SetEase(Ease.OutBack).From(-150).SetDelay(i * 0.0625f);
-        
+
         // Right
-        tip.DOAnchorPosX(-34f, 0.25f).SetEase(Ease.OutBack).From(new Vector2(tip.sizeDelta.x * 2 + 34f, tip.anchoredPosition.y));
+        tip.DOAnchorPosX(-34f, 0.25f).SetEase(Ease.OutBack)
+            .From(new Vector2(tip.sizeDelta.x * 2 + 34f, tip.anchoredPosition.y));
         nextQuestChat.transform.DOScale(Vector2.one, 0.4f).From(Vector2.zero).SetEase(Ease.OutBack).SetDelay(0.1625f);
         nextRewardChat.transform.DOScale(Vector2.one, 0.4f).From(Vector2.zero).SetEase(Ease.OutBack).SetDelay(0.1625f);
-        
+
         CheckRedActivate();
     }
 
@@ -107,18 +107,18 @@ public class View_Lobby : ViewBehaviour
         App.system.player.OnDiamondChange += OnDiamondChange;
         App.system.player.OnUsingIconChange += OnUsingIconChange;
         App.system.player.OnUsingAvatarChange += OnUsingAvatarChange;
-        
+
         App.system.player.OnReduceCoinChange += OnReduceCoinChange;
         App.system.player.OnReduceDiamondChange += OnReduceDiamondChange;
 
         App.system.room.OnRoomsChange += OnRoomsChange;
-        
+
         App.model.dailyQuest.OnQuestsChange += OnQuestsChange;
-        
+
         App.model.lobby.OnTmpMoneyChange += OnTmpMoneyChange;
         App.model.lobby.OnTmpDiamondChange += OnTmpDiamondChange;
         App.model.lobby.OnTmpLevelChange += OnTmpLevelChange;
-        
+
         #region TweenConfig
 
         downButtonsOrigins = new List<Vector2>();
@@ -134,40 +134,28 @@ public class View_Lobby : ViewBehaviour
     {
         int beforeLevel = (int)from;
         int afterLevel = (int)to;
-        
+
         nextRewardChat.SetActive(afterLevel < 40);
-        
-        DOVirtual.DelayedCall(2f, () =>
-        {
-            levelText.text = afterLevel.ToString("00");
-        });
+
+        DOVirtual.DelayedCall(2f, () => { levelText.text = afterLevel.ToString("00"); });
 
         float duration = App.system.tutorial.isTutorial ? 0 : 2.5f;
-        
-        DOVirtual.DelayedCall(duration, () =>
-        {
-            App.system.levelUp.Open(beforeLevel, afterLevel);
-        });
+
+        DOVirtual.DelayedCall(duration, () => { App.system.levelUp.Open(beforeLevel, afterLevel); });
     }
 
     private void OnTmpDiamondChange(object from, object to)
     {
         int diamond = (int)to;
-        DOVirtual.DelayedCall(1.75f, () =>
-        {
-            diamondText.text = diamond.ToString();
-        });
+        DOVirtual.DelayedCall(1.75f, () => { diamondText.text = diamond.ToString(); });
     }
 
     private void OnTmpMoneyChange(object from, object to)
     {
         int money = (int)to;
-        DOVirtual.DelayedCall(1.75f, () =>
-        {
-            coinText.text = money.ToString();
-        });
+        DOVirtual.DelayedCall(1.75f, () => { coinText.text = money.ToString(); });
     }
-    
+
     private void OnReduceDiamondChange(object value)
     {
         diamondText.text = App.system.player.Diamond.ToString();
@@ -185,7 +173,7 @@ public class View_Lobby : ViewBehaviour
 
         if (!App.factory.itemFactory.avatarEffects.ContainsKey(id))
             return;
-        
+
         GameObject effectObject = App.factory.itemFactory.avatarEffects[id];
         Instantiate(effectObject, playerAvatar.transform);
     }
@@ -217,13 +205,14 @@ public class View_Lobby : ViewBehaviour
     private void OnRoomsChange(object value)
     {
         List<Room> rooms = (List<Room>)value;
-        
+
         int count = 0;
         for (int i = 0; i < rooms.Count; i++)
         {
             if (rooms[i].roomData.roomType != RoomType.Features) continue;
             count++;
         }
+
         roomCountText.text = count.ToString("00");
     }
 
@@ -237,10 +226,10 @@ public class View_Lobby : ViewBehaviour
     {
         // 只要上線SetData用
         App.system.player.OnLevelChange -= OnLevelChange;
-        
+
         int level = Convert.ToInt32(value);
         levelText.text = level.ToString("00");
-        
+
         nextRewardChat.SetActive(level + 1 < 40);
     }
 
@@ -268,51 +257,39 @@ public class View_Lobby : ViewBehaviour
             expFill.DOFillAmount(1f / nextExp * nowExp, 0.25f).SetEase(Ease.OutExpo).SetDelay(2f);
             return;
         }
+
         expFill.DOFillAmount(1f / nextExp * nowExp, 0.25f).SetEase(Ease.OutExpo).SetDelay(1.75f);
     }
 
     private void CheckRedActivate() // 檢查紅點有沒有在用
     {
         string animationName = "Play";
-        
+
         if (lobbyDailyQuestRed.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => lobbyDailyQuestRedAnimator.Play(animationName));
-        }
 
         if (lobbyLevelRewardRed.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => lobbyLevelRewardRedAnimator.Play(animationName));
-        }
 
         if (archiveRedPoint.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => archiveRedPointAnimator.Play(animationName));
-        }
 
         if (mailRedPoint.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => mailRedPointAnimator.Play(animationName));
-        }
 
         if (mallRedPoint.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => mallRedPointAnimator.Play(animationName));
-        }
-        
+
         if (bagRedPoint.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => bagRedPointAnimator.Play(animationName));
-        }
-        
+
         if (catRedPoint.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => catRedPointAnimator.Play(animationName));
-        }
-        
+
         if (friendRedPoint.activeSelf)
-        {
             DOVirtual.DelayedCall(0.35f, () => friendRedPointAnimator.Play(animationName));
-        }
+
+        if (eventRedPoint.activeSelf)
+            DOVirtual.DelayedCall(0.35f, () => eventRedPointAnimator.Play(animationName));
     }
 }
